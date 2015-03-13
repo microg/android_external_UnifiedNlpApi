c2032ac640a61440b21f757508558117295fd107
mar-v-in <github@rvin.mooo.com>
2015-03-13 14:21:57 +0100

    Update for gradle layout

--- Android.mk
+++ Android.mk
@@ -2,9 +2,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := UnifiedNlpApi
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_SRC_FILES += src/org/microg/nlp/api/LocationBackend.aidl \
-                   src/org/microg/nlp/api/GeocoderBackend.aidl \
-                   src/org/microg/nlp/api/LocationCallback.aidl
+LOCAL_SRC_FILES := $(call all-java-files-under, unifiednlp-api/src/main/java)
+LOCAL_SRC_FILES += unifiednlp-api/src/main/aidl/org/microg/nlp/api/LocationBackend.aidl \
+                   unifiednlp-api/src/main/aidl/org/microg/nlp/api/GeocoderBackend.aidl \
+                   unifiednlp-api/src/main/aidl/org/microg/nlp/api/LocationCallback.aidl
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
